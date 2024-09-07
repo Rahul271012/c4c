6ef0b1b0d193cdab3d03478edad52dadfc02fdad
NucleoFusion <lakshit.singh.mail@gmail.com>
2024-09-07 17:32:29 +0530

    Addind Route Setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
-import Login from './components/Login'
+import React from "react";
+import Login from "./components/Login";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
     <>
-      <Login/>
+      <Router>
+        <Routes>
+          <Route to="/home" element={<h1>Home</h1>} />
+          <Route to="/" element={<Login />} />
+        </Routes>
+      </Router>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
